a7d1ba0338134c564efd479ec8ed2c597972445f
Shazoji90 <sasongko.firman90@gmail.com>
2023-05-14 03:34:25 +0700

    inisiasi awal

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$todos = [];
+if(isset($_POST['todo'])) {
+    $data = $_POST['todo'];
+    $todos[] = [
+        'todo' => $data,
+        'status' => 0
+    ];
+    file_put_contents('todo.txt', serialize($todos));
+}
+
+?>
+
+<h1>Todo App</h1>
+
+<form method="POST">
+<label>Apa kegiatanmu hari ini?</label><br/>
+<input type="text" name="todo"/>
+<button type="submit">Simpan</button>
+</form>
+
+<ul>
+    <li>
+        <input type="checkbox" name="todo"/>
+        <label>Todo 1</label>
+        <a href="#">hapus</a>
+    </li>
+    <li>
+        <input type="checkbox" name="todo"/>
+        <label>Todo 1</label>
+        <a href="#">hapus</a>
+    </li>
+    <li>
+        <input type="checkbox" name="todo"/>
+        <label>Todo 1</label>
+        <a href="#">hapus</a>
+    </li>
+    <li>
+        <input type="checkbox" name="todo"/>
+        <label>Todo 1</label>
+        <a href="#">hapus</a>
+    </li>
+</ul>
